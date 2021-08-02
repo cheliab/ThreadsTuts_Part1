9a515429a8d262d49b15fae5b396bb73bd9ffe61
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-08-03 09:39:03 +1000

    Накидал класс воркера

---
+++ ThreadsTuts_WpfApp/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace ThreadsTuts_WpfApp
+{
+    public class Worker
+    {
+        private bool _cancelled = false;
+
+        public void Cancel()
+        {
+            _cancelled = true;
+        }
+
+        public void Work()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (_cancelled)
+                    break;
+                
+                Thread.Sleep(50);
+
+                ProcessChanged(i);
+            }
+
+            WorkCompleted(_cancelled);
+        }
+
+        public event Action<int> ProcessChanged;
+
+        public event Action<bool> WorkCompleted;
+    }
+}
