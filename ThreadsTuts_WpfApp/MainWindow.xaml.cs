0beee59d76d6b8761b7c17fbaf0fd1a8c6dc3829
Berezkin Pavel <Berezkin.P@dns-shop.ru>
2021-08-03 23:43:03 +1000

    Добавил вариант с использованием SynchronizationContext

--- ThreadsTuts_WpfApp/MainWindow.xaml.cs
+++ ThreadsTuts_WpfApp/MainWindow.xaml.cs
@@ -22,11 +22,18 @@ namespace ThreadsTuts_WpfApp
     public partial class MainWindow : Window
     {
         private Worker _worker;
+
+        private WorkerSyncContext _workerSyncContext;
+        private SynchronizationContext _context;
         
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += OnLoadWindow;
         }
+
+        #region ВариантБезИспользованияКонтекстаСинхронизации
 
         private void StartButton_OnClick(object sender, RoutedEventArgs e)
         {
@@ -68,5 +75,48 @@ namespace ThreadsTuts_WpfApp
 
             this.InvokeEx(action);
         }
+
+        #endregion
+
+        #region ВариантССинхронизациейКонтекста
+        
+        private void OnLoadWindow(object sender, RoutedEventArgs e)
+        {
+            _context = SynchronizationContext.Current;
+        }
+
+        private void SyncContext_StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _workerSyncContext = new WorkerSyncContext();
+            _workerSyncContext.ProcessChanged += SyncContext_Worker_ProcessChanged;
+            _workerSyncContext.WorkCompleted += SyncContext_Worker_WorkCompleted;
+            
+            SyncContext_startButton.IsEnabled = false;
+            
+            Thread thread = new Thread(_workerSyncContext.Work);
+            thread.Start(_context);
+        }
+
+        private void SyncContext_StopButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _workerSyncContext.Cancel();
+        }
+
+        private void SyncContext_Worker_WorkCompleted(bool cancelled)
+        {
+            SyncContext_mainProgressBar.Value = 0;
+            SyncContext_startButton.IsEnabled = true;
+            
+            string message = cancelled ? "Процесс отменен" : "Процесс завершен!";
+            MessageBox.Show(message);
+        }
+
+        private void SyncContext_Worker_ProcessChanged(int progress)
+        {
+            SyncContext_mainProgressBar.Value = progress;
+        }
+
+        #endregion
+        
     }
 }
---
+++ ThreadsTuts_WpfApp/WorkerSyncContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsTuts_WpfApp
+{
+    public class WorkerSyncContext
+    {
+        private bool _cancelled = false;
+
+        public void Cancel()
+        {
+            _cancelled = true;
+        }
+
+        public void Work(object param)
+        {
+            var context = (SynchronizationContext) param;
+            
+            for (var i = 1; i <= 100; i++)
+            {
+                if (_cancelled)
+                    break;
+
+                Thread.Sleep(50);
+
+                context.Send(OnProgressChanged, i);
+            }
+
+            context.Send(OnWorkComplete, _cancelled);
+        }
+
+        private void OnProgressChanged(object i)
+        {
+            ProcessChanged?.Invoke((int) i);
+        }
+
+        private void OnWorkComplete(object cancelled)
+        {
+            WorkCompleted?.Invoke((bool) cancelled);            
+        }
+
+        public event Action<int> ProcessChanged;
+
+        public event Action<bool> WorkCompleted;
+    }
+}
